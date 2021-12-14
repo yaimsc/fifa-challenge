94a01d89615749e3c061db6b1cb94d3c05b11597
Yaiza <yaimsc@gmail.com>
2021-12-14 01:27:53 +0100

    get teams

--- src/pages/SelectTeam.js
+++ src/pages/SelectTeam.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MenuItem } from '@mui/material';
+import { getListItemUtilityClass, MenuItem } from '@mui/material';
 import { DropdownSelect } from '../components/common';
 
 const SelectTeam = () => {
@@ -7,29 +7,32 @@ const SelectTeam = () => {
     useEffect(() => {
         fetch('https://api.football-data.org/v2/competitions/2021/teams', {
             method: 'GET',
+            mode: 'cors',
             headers: {
                 'X-Auth-Token': '842eabacaca542ec9ed31f058efc2115'
             }
             })
-            .then(response => console.log(response.json()))
-            .then(function(data){
-                console.log(data)
-                // setTeams(data)
-            })
+            .then(response => response.json())
+            .then(data => setTeams(data.teams))
             .catch(error => error)
         })
-    console.log(teams)
+    console.log(teams);
+
     return(
-        <DropdownSelect
-            label="Select Team"
-            list={teams}
-        >
-        {teams.map((data, index) => (
-            <MenuItem key={index} value={data.id}>
-                {data.name}
-            </MenuItem>
-            ))}
-        </DropdownSelect>
+        // <DropdownSelect
+        //     label="Select Team"
+        //     list={teams}
+        // >
+        <>
+        {teams.map((value, key) => (
+                console.log(key),
+                <MenuItem key={key} value={value.id}>
+                {value.name}
+                </MenuItem>
+            ))
+        }
+        </>
+        // </DropdownSelect>
     )
 }
 
